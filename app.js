c8d6e867f5aba6d158f3920ff4d3f68a5e632081
Noah Serrault <serraultn@gmail.com>
2026-06-09 14:14:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -275,8 +275,8 @@ function displayResult(row) {
         badgesHtml += `<div class="badge location"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg> Loc: ${Location}</div>`;
     }
     
-    if (AGM && AGM.toLowerCase() === 'yes') {
-        badgesHtml += `<div class="badge agm"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/></svg> AGM Recommended</div>`;
+    if (AGM) {
+        badgesHtml += `<div class="badge agm"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/></svg> AGM: ${AGM}</div>`;
     }
     
     if (Difficult) {
